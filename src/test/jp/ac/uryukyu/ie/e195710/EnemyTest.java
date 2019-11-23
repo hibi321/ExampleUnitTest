a2dfcdf3d71f77dd76416e4d1b96323a6bf23054
Hibiki Miyagi <e195710@ie.u-ryukyu.ac.jp>
2019-11-23 20:49:00 +0900

    1st commit

---
+++ src/test/jp/ac/uryukyu/ie/e195710/EnemyTest.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e195710;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void attack() {
+    }
+}
